76052cf9c8ed826305bd2dd0b74ee9a2c693a4fc
HassanSaeed7 <Hsaeed1007@gmail.com>
2022-03-02 21:35:59 -0800

    styling projects and hero

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import './css/style.css'
+
+const targets = document.querySelectorAll('[data-src]')
+
+const lazyLoading = target => {
+    const newObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                const src = img.getAttribute('data-src');
+                img.setAttribute('src', src);
+                img.classList.add('fade');
+                observer.disconnect();
+            }
+        })
+    })
+    newObserver.observe(target)
+}
+
+targets.forEach(lazyLoading);
+
+
+
+    
