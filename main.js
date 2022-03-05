f1c771e0d8f8345444cdb3d2623ee6284a09240f
HassanSaeed7 <Hsaeed1007@gmail.com>
2022-03-05 01:12:49 -0800

    predeploy commit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './css/style.css'
-
-const targets = document.querySelectorAll('[data-src]')
+const targets = document.querySelectorAll('[data-src]');
+const sections = document.querySelectorAll('.container');
+const body = document.body;
+let lastScroll = 0;
 
 const lazyLoading = target => {
     const newObserver = new IntersectionObserver((entries, observer) => {
@@ -14,11 +16,46 @@ const lazyLoading = target => {
             }
         })
     })
-    newObserver.observe(target)
+    newObserver.observe(target);
 }
-
 targets.forEach(lazyLoading);
 
 
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
 
+    if (currentScroll <= 0) {
+        body.classList.remove('scroll-up')
+    };
+    if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-up')
+        body.classList.add('scroll-down')
+    }
+    if (currentScroll < lastScroll && !body.classList.contains('scroll-up')) {
+        body.classList.add('scroll-up')
+        body.classList.remove('scroll-down')
+    }
+    lastScroll = currentScroll;
+
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (currentScroll >= (sectionTop)) {
+            current = section.getAttribute('id');
+        }
+    });
+    const navLi = document.querySelectorAll('.navLinks');
+    navLi.forEach(item => {
+		item.classList.remove('active');
+		const href = item.getAttribute('href').substring(1);
+		if (href === current) {
+			item.classList.add('active');
+		};
+	});
+})
     
+
+
+
+
+
